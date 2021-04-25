1dfaabf7435cc3dc81deecef81f5e96e118766ee
MariamNesiem <mariam.nesiem@yahoo.com>
2021-04-25 12:39:36 +0200

    Project

---
+++ MethodChaining/Calculation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodChaining
+{
+    class Calculation
+    {
+        private float MyNumber = 0;
+        public string Message="";
+        public Calculation(float number)
+        {
+                this.MyNumber = number;
+        }
+
+        public Calculation Addition(float number)
+        {
+                this.MyNumber += number;
+            return this;
+        }
+
+        public Calculation Subtraction(float number)
+        {
+                this.MyNumber -= number;
+            return this;
+        }
+
+        public Calculation Division(float number)
+        {
+            this.MyNumber = number == 0 ?  0: this.MyNumber/number;
+            return this;
+        }
+
+        public Calculation Multiplication(float number)
+        {
+                this.MyNumber *= number;
+            return this;
+        }
+
+
+        public float Result()
+        {
+            return this.MyNumber;
+        }
+    }
+}
---
+++ MethodChaining/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MethodChaining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation cal = new Calculation(5);
+            float result = cal.Addition(5)
+                            .Addition(5)
+                            .Multiplication(3)
+                            .Division(6)
+                            .Subtraction(3)
+                            .Result();
+            Console.WriteLine("The Result: "+ result);
+        }
+    }
+}
